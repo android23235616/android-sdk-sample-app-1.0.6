1ce18a7ac4121920fd49c824a0eadde8fb49a5bd
android23235616 <majumdartanmay68@gmail.com>
2018-03-19 20:24:15 +0530

    newone

--- app/src/main/java/com/instamojo/androidsdksample/login.java
+++ app/src/main/java/com/instamojo/androidsdksample/login.java
@@ -1,10 +1,20 @@
 package com.instamojo.androidsdksample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.w3c.dom.Text;
 
@@ -15,7 +25,8 @@ public class login extends AppCompatActivity {
     //email
     //name
     //mobile number
-    TextView phone,email,name,password;Button register;
+    EditText name,password;
+    Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,14 +39,41 @@ public class login extends AppCompatActivity {
             public void onClick(View view) {
                 if(check_internet_connection())
                 {
-                    register_python();
+                    String ssoid=name.getText().toString();
+                    String pass=password.getText().toString();
+                    if(ssoid.trim().length()>0 && pass.trim().length()>0)
+                    {
+                        register_python(ssoid,pass);
+                    }
+                    else
+                    {
+                        Toast.makeText(login.this, "Kindly enter the proper details", Toast.LENGTH_SHORT).show();
+                    }
+
                 }
             }
         });
 
     }
 
-    private void register_python() {
+    private void register_python(String ssoid, String pass) {
+
+
+        StringRequest strngrqst=new StringRequest(Request.Method.GET, "", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        RequestQueue a= Volley.newRequestQueue(getApplicationContext());
+        a.add(strngrqst);
+
     }
 
     private boolean check_internet_connection() {
@@ -52,8 +90,8 @@ public class login extends AppCompatActivity {
 
     private void initialize() {
 
-        name=(TextView) findViewById(R.id.name);
-        password=(TextView) findViewById(R.id.password);
+        name=(EditText) findViewById(R.id.name);
+        password=(EditText) findViewById(R.id.password);
         register=(Button) findViewById(R.id.Register);
 
     }
